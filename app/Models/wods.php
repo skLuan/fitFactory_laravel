6904c616f6765a01c102d297638495689fed3528
Luan E <luan.erazo@skyrocketlab.space>
2023-02-27 22:44:48 -0500

    creados los modelos o entidades, migraciones y seeders

---
+++ app/Models/wods.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class wods extends Model
+{
+    use HasFactory;
+}
