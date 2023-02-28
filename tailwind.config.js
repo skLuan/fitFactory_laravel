ca69a61101cad0a5711ceb90932bb9e1fdc8ffa0
Luan E <luan.erazo@skyrocketlab.space>
2023-02-27 23:03:04 -0500

    logo in home

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,47 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                ffGray: {
+                    DEFAULT: "#BBB3AF",
+                },
+                ffGreen: {
+                    DEFAULT: "#92F663",
+                    black: "#071301",
+                    white: "#E4FDD8",
+                },
+                ffRed: {
+                    DEFAULT: "#F6503A",
+                    black: "#140301",
+                    white: "#FDDCD8",
+                },
+                ffYellow: {
+                    DEFAULT: "#F8C81E",
+                    black: "#141001",
+                    white: "#FEF5D7",
+                },
+                ffBlack: {
+                    DEFAULT: "#07070A",
+                    mid: "#272A31",
+                    high: "#202020",
+                },
+                ffWhite: {
+                    DEFAULT: "#FCFCFC",
+                    mid: "#E6E6E6",
+                },
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [require("@tailwindcss/forms")],
 };
